e43976f73f8ddc9525001951b6ca89e8ec3652e6
Brian Walker Jr <thaballa79@gmail.com>
2022-09-29 18:01:16 -0500

    updates auth page label

--- my_colors_web/lib/pages/sign_in_page.dart
+++ my_colors_web/lib/pages/sign_in_page.dart
@@ -45,7 +45,7 @@ class _SignInPageState extends State<SignInPage> {
                         style:
                             TextStyle(color: MyColor.blueishIdk, fontSize: 30)),
                     const SizedBox(height: 15),
-                    Text("Access your Favs via\nweb or Android",
+                    Text("Access your Favs\nvia web or Android",
                         style:
                             TextStyle(color: MyColor.blueishIdk, fontSize: 14)),
                     const SizedBox(height: 40),
